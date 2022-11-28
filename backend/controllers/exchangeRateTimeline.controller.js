77e8285306a4d84ddabc5d09fb38864aad37e37c
marios <marios.vlachog@gmail.com>
2022-11-28 15:17:31 +0200

    add new exchangeRate

--- backend/controllers/exchangeRateTimeline.controller.js
+++ backend/controllers/exchangeRateTimeline.controller.js
@@ -1,24 +1,31 @@
 
 import sequelize from "../models/index.js";
-
+import { Op } from 'sequelize';
 
 
 // GET exchange/
 export const getExchanges = async (req, res, next) => {
     try {
         const { from, to, latest } = req.query;
+        console.log(typeof from, typeof to); // ids
         
         const exchanges = await sequelize.models.ExchangeRateTimeline.findAll({
+            attributes: ['id', 'rate', 'updatedAt'],
             include: {
                 model: sequelize.models.ExchangeRate,
+                attributes: ['comboKey'],
                 required: true,
-                include: [{model: sequelize.models.Currency, as: 'from'}, {model: sequelize.models.Currency, as: 'to'}],
+                include: [
+                    {model: sequelize.models.Currency, as: 'from', attributes: ['id', 'name'],}, 
+                    {model: sequelize.models.Currency, as: 'to', attributes: ['id', 'name'],}
+                ],
                 where: {
-                    fromId: 1,
-                    toId: 2
+                    [Op.or]: [
+                        {[Op.and]: [{fromId: from}, {toId: to,}]}, 
+                        {[Op.and]: [{fromId: to}, {toId: from,}]}
+                    ]
                 }
-            },
-                        
+            },            
         })
 
         res.status(200).json( exchanges );
@@ -34,12 +41,30 @@ export const createExchange = async (req, res, next) => {
 
 
     try {
-        
+        const { from, to, rate } = req.body;
+        const foundExchange = await sequelize.models.ExchangeRate.findOne({
+            attributes: ['id', 'fromId', 'toId', 'comboKey'],
+            where: {
+                [Op.or]: [
+                    {[Op.and]: [{fromId: from}, {toId: to,}]}, 
+                    {[Op.and]: [{fromId: to}, {toId: from,}]}
+                ]
+            }
+        });
 
-        res.status(200).json({ 'message': 'currency created' });
+        let reverse = false;
+        if (foundExchange.fromId === to && foundExchange.toId === from) {
+            reverse = true;
+        }
+
+        const createdExchange = await sequelize.models.ExchangeRateTimeline.create({
+            exchangeRateId: foundExchange.id,
+            rate: reverse ? 1/rate : rate
+        })
+
+        res.status(200).json({ 'message': 'Exchange Updated successfully' });
 
     } catch (err) {
-        await t.rollback();
         res.status(404).json({ 'message': err.message });
     } 
 }
--- backend/routes/exchange.routes.js
+++ backend/routes/exchange.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getExchanges } from '../controllers/exchangeRateTimeline.controller.js';
+import { createExchange, getExchanges } from '../controllers/exchangeRateTimeline.controller.js';
 import { verifyAccessToken } from '../middlewares/verifyAccessToken.js';
 
 const router = express.Router();
 
 // all routes here start with /exchange
 router.get('/',  getExchanges);
+
+router.post('/',  createExchange);
 
 export default router;
